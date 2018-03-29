fb34077a13d5756b1be75df479b98ff3547f02ac
lorizh <14786762+lorizh@users.noreply.github.com>
2018-03-29 09:45:06 -0600

    Create navigation.php

---
+++ navigation.php
@@ -0,0 +1,13 @@
+<div id="navigation">
+<ul>
+<li<?php if ($thisPage=="HOME") 
+echo " id=\"currentpage\""; ?>>
+<a href="#top">HOME</a></li>
+<li<?php if ($thisPage=="ABOUT") 
+echo " id=\"currentpage\""; ?>>
+<a href="#two">ABOUT</a></li>
+<li<?php if ($thisPage=="PORTFOLIO") 
+echo " id=\"currentpage\""; ?>>
+<a href="/portfolio">PORTFOLIO</a></li>
+</ul>
+</div>
